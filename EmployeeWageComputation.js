892fd8b04870cca72e607b23cb45568165c94bbf
shaheen5 <miyashaheen1995@gmail.com>
2021-05-17 20:01:21 +0530

    Added : Code To Check Employee Presence

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -1,1 +1,11 @@
 console.log("WELCOME TO EMPLOYEE WAGE COMPUTATION PROBLEM");
+//declare constant
+const IS_ABSENT = 0;
+//check employee presence using random function
+let empCheck = Math.floor(Math.random()*10) %2 ;
+if (empCheck == IS_ABSENT){
+    console.log("Employee is ABSENT");
+    return;
+}else{
+    console.log("Employee is PRESENT");
+}
