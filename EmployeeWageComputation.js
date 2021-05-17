21bcf9bb0d02836195547d0e5b53fd2f980333d3
shaheen5 <miyashaheen1995@gmail.com>
2021-05-17 23:19:42 +0530

    Added : Code To Calculate Emp Wages Till Max Work Hours

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -7,6 +7,8 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
+
 
 //function to get work hours
 function getWorkingHours(empCheck) {
@@ -21,16 +23,17 @@ function getWorkingHours(empCheck) {
 }
 
 //declare variables
-let empHrs = 0 ;
-let empWage = 0 ;
+let totalEmpHrs = 0 ;
+let totalWorkingDays = 0 ;
 
-//calculating wages for a month
-for(let day = 0; day < NUM_OF_WORKING_DAYS ; day++){
+//calculating wages for a month or till no. of working hours 
+while (totalEmpHrs <= MAX_HRS_IN_MONTH  && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
     //check employee presence using random function
     let empCheck = Math.floor(Math.random()*10) % 3 ;
-    //call function
-    empHrs += getWorkingHours(empCheck);
+    //call function to get employee work hours
+    totalEmpHrs += getWorkingHours(empCheck);
 }
-//calculate daily wage
-empWage = empHrs * WAGE_PER_HOUR ;
-console.log("Total Hours : "+empHrs+" Employee Wage : "+empWage);
+//calculate wage
+let empWage = totalEmpHrs * WAGE_PER_HOUR ;
+console.log("Total Days : "+totalWorkingDays+" Total Hours : "+totalEmpHrs+" Employee Wage : "+empWage);
